f98e176ce09d6f1367b8888161646c11e000e18a
Dmitriy Trunov <dtrunov.rus@gmail.com>
2015-09-19 21:30:01 +0700

    Updated file for Lesson 3.

--- index.php
+++ index.php
@@ -1,41 +1,26 @@
 <?php
 
-error_reporting(E_ALL|E_ERROR|E_PARSE|E_WARNING);
-ini_set('display_errors',1);
+error_reporting(E_ALL | E_ERROR | E_PARSE | E_WARNING);
+ini_set('display_errors', 1);
 
 /* Задание 1 */
-$name = 'Дмитрий';
-$age = 27;
-echo "Меня зовут $name <br/>Мне $age лет";
-echo "<br/><br/>";
+$date = array();    
+$days = array();    
+$months = array();
 
-unset($name);
-unset($age);
-
-/* Задание 2 */
-define('MYCITY','Новосибирск');
-If (MYCITY){
-    echo MYCITY;
+echo ("В сгенерированном массиве получились даты:<br>");
+for ($i = 0; $i < 5; $i++) {
+    $date[] = rand(0, time());                          
+    $days[$i] = (int)(date('d', $date[$i]));        
+    $months[$i] = (int)date('m', $date[$i]);        
+    echo (date('d.m.Y', $date[$i]) . "<br>");
 }
-define('MYCITY','Красноярск');
-echo "</br><br/>";
+echo ("Среди них ".min($days)." - минимальный день, ".max($months)."- максимальный месяц<br><br>");
 
-/* Задание 3 */
-$book = array();
-$book['title'] = 'Ночь Дракона';
-$book['author'] = 'Ричард А.Кнаак';
-$book['pages'] = 500;
-echo 'Недавно я прочитал книгу '.$book['title'].
-     ', написанную автором '.$book['author'].'. '.
-     'Я осилил все '.$book['pages'].
-     ' страниц. Мне она очень понравилась.';
-echo "</br></br>";
+sort($date);
+$selected = array_pop($date);
+echo('Переменная $selected без смены часового пояса: '.date('d.m.Y H:i:s',$selected)."<br>");
+date_default_timezone_set('America/New_York');
+echo('Переменная $selected в часовом поясе America/New_York: '.date('d.m.Y H:i:s',$selected));
 
-/* Задание 4 */
-$books[] = array ('title'=>'Круг Ненависти', 'author'=>'Кейт ДеКандидо', 'pages'=>500);
-$books[] = array ('title'=>'Душа Демона', 'author'=>'Ричард А.Кнаак', 'pages'=>500);
-echo 'Недавно я прочитал книги '.$books[0]['title'].' и '.$books[1]['title'].
-     ' написанные соответственно '.$books[0]['author'].' и '.$books[1]['author'].'. '.
-     'Я осилил в сумме '.($books[0]['pages']+$books[1]['pages']).
-     ' страниц. Не ожидал от себя такого.';
 ?>
